6ad7084a9aba4b88b6d26204dea25233ea1c53a6
pariya <negahdari.ce.shirazu@gmail.com>
2021-06-02 23:44:56 +0430

    Additional documentation on fuzzing for lists

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from examples.symbolic_fuzzer_unsat_core import symbolic_fuzzer_unsat_core
 from examples.symbolic_fuzzer_abs import symbolic_fuzzer_abs
 
 if __name__ == "__main__":
-    symbolic_fuzzer()
-    #symbolic_fuzzer_array()
+    #symbolic_fuzzer()
+    symbolic_fuzzer_array()
     #symbolic_fuzzer_for_loop()
     #symbolic_fuzzer_unsat_core()
     #symbolic_fuzzer_abs()
